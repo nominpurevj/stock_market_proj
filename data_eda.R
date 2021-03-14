e3981d6c9b59321db4c2455cf00b5a4fe8194869
nomin <nominpurevj@gmail.com>
2021-03-14 15:43:07 -0700

    updated comments

--- data_eda.R
+++ data_eda.R
@@ -3,16 +3,18 @@ library(GGally)
 # create a scatterplot matrix
 ggscatmat(spy.us, color = "direction")
 
-# understandably, strong correlations are observed among the volumes of a given day 
-# and the days immediately preceding it. For instance, volume.lag2 and volume.lag3 have a correlation
-# of 0.85
+# Understandably, strong correlations are observed among the volumes of a 
+# given day and the days immediately preceding it. For instance, volume.lag2 
+# and volume.lag3 have a correlation of 0.85.
 
-# moreover, volume.lag1 and lag2 have weak correlations. The same goes for volume.lag2 and lag3.
-# And the same goes for volume.lag3 and lag4. They hint that smaller returns in the market on a
-# given day are associated with higher volume of trading on the next day. 
+# Moreover, volume.lag1 and lag2 have a weak correlation. Even though the 
+# correlation is weak, it is worth noting as most variables are not correlated
+# at all. The same goes for volume.lag2+lag3 and volume.lag3+lag4. They hint 
+# that smaller returns in the market on a given day are associated with higher
+# volume of trading on the next day. 
 
-# lets take a closer look at the relationship between volume and returns.
-
+# Let's take a look at the relationship between volume and returns to get a 
+# clearer understanding of the correlation.
 ggplot(spy.us, aes(lag2, volume.lag1)) +
   geom_point() + 
   geom_smooth(se = FALSE)
